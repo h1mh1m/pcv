c38562642935af538217ae8ee3d001cff14df596
h1mh1m <nraushanfekr@gmail.com>
2026-09-09 18:26:59 +0700

    first

---
+++ main.py
@@ -0,0 +1,42 @@
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# image = cv2.imread("foto/images.jpeg")
+
+# [h,w,c] = image.shape
+# for i in range(h):
+#     for j in range(w):
+#         image[i,j,0] = 0
+#         image[i,j,2] = 0
+
+# # plt.imshow(image)
+# # plt.title("plt")
+# # plt.show()
+
+# cv2.imshow("images",image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# cv2.waitKey(1)
+
+video = cv2.VideoCapture(0)
+
+if not video.isOpened():
+    print("gagal membuka kamera")
+    exit
+
+while True:
+    ret, frame = video.read()
+
+    if not ret :
+        print("gagal mendapatkan frame dari kamera")
+        break
+
+    cv2.imshow("Kamera",frame)
+
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
